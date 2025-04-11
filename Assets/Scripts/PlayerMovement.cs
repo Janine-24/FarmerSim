beb54d4efdd4a451a9ca32a718adef7352c27520
Chung Sun <chungsunlee06@gmail.com>
2025-04-11 14:53:56 +0800

    Add: Player basic movement animation (first level)

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,20 +1,46 @@
 using UnityEngine;
 
+
+//player movement
 public class PlayerMovement : MonoBehaviour
 {
     public float moveSpeed = 5f;
     private Rigidbody2D rb;
     private Vector2 movement;
+    private Animator animator;
+    private SpriteRenderer spriteRenderer;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        //get sprite renderer
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     void Update()
     {
+        //get input
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
+
+        //update animator parameters
+        animator.SetFloat("MoveX",movement.x);
+        animator.SetFloat("MoveY",movement.y);
+        animator.SetBool("IsMoving", movement != Vector2.zero);
+
+        if (movement.x > 0)
+        {
+            spriteRenderer.flipX = false;
+        }
+        else if (movement.x < 0)
+        {
+            spriteRenderer.flipX = true;
+        }
+        else
+        {
+            spriteRenderer.flipX = false;
+        }
     }
 
     void FixedUpdate()
