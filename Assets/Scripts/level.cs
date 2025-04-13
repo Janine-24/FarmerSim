3a9bc0d840d9a78b3369f731063099b9c5313112
Gohpeijia <peijia0307@gmail.com>
2025-04-13 20:56:28 +0800

    character substituded,map done(added obstacle),HUD done exept machien (waiting the time taken for each machine)

---
+++ Assets/Scripts/level.cs
@@ -0,0 +1,64 @@
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+using UnityEngine.Audio;
+
+public class LevelSystem : MonoBehaviour
+
+{
+    public int level = 1;
+    public int currentXP = 0;
+    public int xpToNextLevel = 100;
+
+    public TextMeshProUGUI levelText;
+    public Slider xpBar;
+    public AudioSource audioSource;
+    public AudioClip xpSound;
+    public AudioClip levelUpSound;
+    public GameObject levelUpPopup;
+
+
+    void Start()
+    {
+        UpdateUI();
+    }
+
+    public void AddXP(int amount)
+    {
+        currentXP += amount;
+        if (xpSound != null && audioSource != null)
+            audioSource.PlayOneShot(xpSound);
+        if (currentXP >= xpToNextLevel)
+        {
+            currentXP -= xpToNextLevel;
+            LevelUp();
+        }
+
+        UpdateUI();
+    }
+
+    void LevelUp()
+    {
+        level++;
+        xpToNextLevel += 100; //  can increase XP requirement per level
+        if (audioSource != null && levelUpSound != null)
+        {
+            audioSource.PlayOneShot(levelUpSound);
+        }
+        Debug.Log("Level Up! Now Level: " + level);
+        if (levelUpPopup != null)
+        {
+            levelUpPopup.SetActive(true);
+            Invoke("HidePopup", 2f);
+        }
+
+        UpdateUI();
+    }
+
+    void UpdateUI()
+    {
+        levelText.text = "Level " + level;
+        xpBar.maxValue = xpToNextLevel;
+        xpBar.value = currentXP;
+    }
+}
