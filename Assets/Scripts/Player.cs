3f96874af22e8a4d8236235cd0b23385277839fa
Chung Sun <chungsunlee06@gmail.com>
2025-04-29 22:54:27 +0800

    plowing mechanism

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Vector3Int position = new Vector3Int((int)transform.position.x, (int)transform.position.y, 0);
+
+            if (GameManager.instance.tileManager.IsInteractable(position))
+            {
+                Debug.Log("Tile is interactable");
+                GameManager.instance.tileManager.SetInteracted(position);
+            }
+            else
+            {
+                GameManager.instance.tileManager.ReverseInteracted( position );
+            }
+        }
+    }
+}
--- Assets/Scripts/SeedItem.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "NewSeed", menuName = "Farming/Seed")]
-public class SeedItem : ScriptableObject
-{
-    public string seedName;
-    public Sprite icon;
-    public GameObject cropPrefab;
-}
